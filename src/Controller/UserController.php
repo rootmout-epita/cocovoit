299416926119443fdb2cb0bac237e5b757994df3
Pierre KELBERT <pierre.kelbert@kleeft.fr>
2019-03-24 18:52:44 -0400

    Creation of UserController skeleton

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,16 +5,66 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route("/account")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * Show the login form for connection.
+     *
+     * @Route("/login" name="user.login")
      */
-    public function index()
+    public function login()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        //TODO
+    }
+
+
+    /**
+     * Logout the user, simple…
+     *
+     * @Route("/logout" name="user.logout")
+     */
+    public function logout()
+    {
+        //TODO
+
+        //attention, a part une simple redirection rien n'est demandé de plus. Symfony gère lui même la déconnexion.
+    }
+
+
+    /**
+     * Registration form.
+     *
+     * @Route("/register", name="user.registration")
+     */
+    public function register()
+    {
+        //TODO
+    }
+
+
+    /**
+     * Main page of the account section. Show all reservations made by user.
+     *
+     * @Route("/reservations", name="user.dashboard.reservations")
+     */
+    public function showReservations()
+    {
+        //TODO
+    }
+
+
+    /**
+     * Second page of the dashboard. Show the trips created by user.
+     *
+     * @Route("/trips", name="user.dashboard.trips")
+     */
+    public function showTrips()
+    {
+        //TODO
     }
 }
