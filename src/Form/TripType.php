e9020d1d99f782a272105b28b969107d46b286fd
Clement <Clement@Clement.localdomain>
2019-04-06 18:16:08 -0400

    fix error

---
+++ src/Form/TripType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trip;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TripType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('departure_place')
+            ->add('arrival_place')
+            ->add('departure_schedule')
+            ->add('duration')
+            ->add('canceled')
+            ->add('nbr_places')
+            ->add('price')
+            ->add('conductor')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trip::class,
+        ]);
+    }
+}
