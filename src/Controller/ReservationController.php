653e8bb487fff511173d2117292da4d746e1a4d6
Hugo DI GUARDIA <hugo.diguardia@gmail.com>
2019-04-05 20:25:55 -0400

    Hugo : Deleted deprecated methods from ReservationController

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -15,63 +15,6 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ReservationController extends AbstractController
 {
-    /**
-<<<<<<< HEAD
-     * @Route("/new", name="reservation.new")
-     *
-     * Crée une nouvelle réservation si l'utilisateur est connecté.
-     *
-     * @param Request $request : La requête à traiter lorsque l'utilisateur a appuyé sur le bouton "Réserver"
-     *
-     * @return Response
-     *
-     * @author hdiguardia
-     */
-    public function new(Request $request)
-    {
-        //TODO
-        // Crée un formulaire qui va gérer la requête envoyée par l'utilisateur
-        $reservation = new Reservation();
-        /*$form = $this->createForm(ReservationFormType::class, $reservation); // CREER le formulaire de réservation
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            // Vérifie si l'utilisateur est bien connecté
-            $isFullyAuthenticated = $this->get('security.context')
-                ->isGranted('IS_AUTHENTICATED_FULLY');
-            if(!$isFullyAuthenticated)
-            {
-                $this->addFlash('error', 'Veuillez vous connecter afin de réserver un voyage.');
-                return $this->redirectToRoute('trip.view');
-            }
-
-            // S'il n'y a aucun problème, crée une nouvelle réservation
-            $reservation->setTrip(/trip/reserver/{id}); // Pas sûr de savoir comment avoir le Trip à réserver
-            $reservation->setClient($this->getUser()->getId());
-            $this->em->persist($account);
-            $this->em->flush();
-            $this->addFlash('success', 'Le voyage a bien été réservé.');
-            return $this->redirectToRoute('user.dashboard.reservations');
-        }
-
-        return $this->render('accounts/new.html.twig', [
-            'account' => $account,
-            'form' => $form->createView(),
-        ]);*/
-
-
-    }
-
-
-    /**
-     * @Route("/delete", name="reservation.delete")
-     */
-    public function delete()
-    {
-        //TODO
-    }
-
 
     /**
      * @Route("/check/{reservation_key}", name="reservation.check")
